34550b873084cb1df43df1ab105b424fa6ab7f48
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-08-03 11:08:24 +0200

    <feat>Node.js API Gateway is up

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+// Import the express library
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Gateway Service!');
+});
+
+app.listen(port, () => {
+  console.log(`Gateway service listening at http://localhost:${port}`);
+});
