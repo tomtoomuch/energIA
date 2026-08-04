a100482d6b12908ac61530a6d306a4cbba4118b6
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-08-04 10:37:16 +0200

    <feat>tests fastAPI

--- ms-python/app/main.py
+++ ms-python/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 @app.get("/")
 def read_root():
